a54301fdcdebfd850050acf5ed38dce1fedfbd22
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-10 15:14:51 +0600

    [DONE] create or add todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dtos/todo.dto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+  @Post('add')
+  async addTodo(@Body() payload: CreateTodoDto) {
+    const result = await this.todoService.createTodo(payload);
+    return {
+      status: HttpStatus.CREATED,
+      message: 'Todo is added',
+      data: result,
+    };
+  }
+}
