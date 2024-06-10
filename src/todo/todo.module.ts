69bf4504338cf3cddb91b0da69ab0d619c98c3a5
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-10 14:29:02 +0600

    db config done

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ToDo, todoSchema } from 'src/schemas/todo.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: ToDo.name,
+        schema: todoSchema,
+      },
+    ]),
+  ],
   controllers: [TodoController],
   providers: [TodoService],
 })
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ToDo } from 'src/schemas/todo.schema';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(@InjectModel(ToDo.name) private todoModel: Model<ToDo>) {}
+}
